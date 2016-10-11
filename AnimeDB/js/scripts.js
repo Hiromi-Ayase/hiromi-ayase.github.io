a8cd13f9eecf113ac3cb2ee58e61106de00927ec
Hiromi Ayase <hiromi.ayase@gmail.com>
2016-10-12 03:21:39 +0900

    Added youtube search.

--- AnimeDB/js/scripts.js
+++ AnimeDB/js/scripts.js
@@ -2,7 +2,7 @@ var list = [];
 for (var i = 0; i < rawData.length; i ++) {
     var d = rawData[i];
     var start = d.started_year + "/" + d.started_month + "/" + d.started_day;
-    list[i] = [d.id, d.madb_id, d.medium, start, d.broadcast, d.author, "<a href=\"" + d.madb_uri + "\">link</a>"];
+    list[i] = [d.id, d.madb_id, d.medium, start, d.broadcast, d.author, "<a href=\"https://www.youtube.com/results?search_query=" + encodeURIComponent(d.title) + "\">Youtubeで見る</a>", "<a href=\"" + d.madb_uri + "\">link</a>"];
 }
 $(document)
         .ready(
@@ -29,7 +29,7 @@ $(document)
                             list, // 検索対象のリスト
                             {
                                 dispMax : 10, // オプション
-                                startElementText : '<table class="table table-bordered"><tr><th>タイトル</th><th>Media Art DB ID</th><th>メディア</th><th>放送開始日</th><th>配信</th><th>作者</th><th>メディアDB</th>',
+                                startElementText : '<table class="table table-bordered"><tr><th>タイトル</th><th>Media Art DB ID</th><th>メディア</th><th>放送開始日</th><th>配信</th><th>作者</th><th>Youtube検索</th><th>メディアDB</th>',
                                 searchBefore : function () {
                                     status.innerHTML = 'search...';
                                 },
